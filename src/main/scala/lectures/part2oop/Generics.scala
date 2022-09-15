ec14cfbf7f8a72ae5f6a9186be8e9083d1218881
Vitor Gabriel Carrilho <vitorg.carrilho@gmail.com>
2022-09-16 00:01:02 +0100

    Covariance, invariance and contravariance

---
+++ src/main/scala/lectures/part2oop/Generics.scala
@@ -0,0 +1,31 @@
+package lectures.part2oop
+
+object Generics extends App {
+
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+
+
+  // COVARIANCE
+
+  class CovariantGeneric[+T]
+
+  val animal: Animal = new Cat
+  val covariantGeneric: CovariantGeneric[Animal] = new CovariantGeneric[Cat]
+  val covariantGeneric2: CovariantGeneric[Animal] = new CovariantGeneric[Dog]
+
+
+  // INVARIANCE
+  class InvariantGeneric[T]
+  // val invariantGeneric: InvariantGeneric[Animal] = new InvariantGeneric[Cat] -> compile error
+  val invariantGeneric: InvariantGeneric[Animal] = new InvariantGeneric[Animal]
+
+  // CONTRAVARIANCE
+
+  class Contravariance[-T]
+
+  val contravariance: Contravariance[Cat] = new Contravariance[Animal]
+
+}
